786b238958132fe66347e12550832728ede59d93
21Coochie <brghtmalone@gmail.com>
2024-06-02 14:02:55 +0000

    added triangle type identifier

---
+++ triangle-type-identifier/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side 1: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side 2: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side 3: ");
+            double side3 = Convert.ToDouble(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+            {
+                Console.WriteLine("Triangle type: Equilateral");
+            }
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+            {
+                Console.WriteLine("Triangle type: Isosceles");
+            }
+            else
+            {
+                Console.WriteLine("Triangle type: Scalene");
+            }
+        }
+    }
+}
